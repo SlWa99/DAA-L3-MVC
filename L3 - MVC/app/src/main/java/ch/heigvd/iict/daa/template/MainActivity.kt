9d3f4112cbaa48c8bdadd4a584f72af53588beeb
Simeline <valentin.bugna@heig-vd.ch>
2024-10-22 17:19:03 +0200

    first version ... first picture ... not analyzed

--- L3 - MVC/app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
+++ L3 - MVC/app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
@@ -1,11 +1,29 @@
 package ch.heigvd.iict.daa.template
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialiser le Spinner
+        val spinner: Spinner = findViewById(R.id.spinnerNationalite)
+
+        // Créer un ArrayAdapter en utilisant le fichier de ressources de chaînes
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            this,
+            R.array.nationalities, // Assurez-vous que ce tableau est défini dans res/values/strings.xml
+            android.R.layout.simple_spinner_item
+        )
+
+        // Spécifier le layout à utiliser quand la liste d'options s'affiche
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Appliquer l'adapter à le Spinner
+        spinner.adapter = adapter
     }
 }
